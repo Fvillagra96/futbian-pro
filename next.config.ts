8a3d9c8aadac7ef3ae50eaeb52e81af45f9153f5
Fvillagra96 <fvillagra.mendez@gmail.com>
2026-04-13 14:52:12 -0400

    Corrigiendo estructura de next.config y pwa

--- next.config.ts
+++ next.config.ts
@@ -1,17 +1,19 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
-};
-
-export default nextConfig;
 const withPWA = require('next-pwa')({
   dest: 'public',
   register: true,
   skipWaiting: true,
-  disable: process.env.NODE_ENV === 'development' // Solo se activa en producción
+  disable: process.env.NODE_ENV === 'development',
 });
 
-module.exports = withPWA({
-  // Tus otras configuraciones si las tienes
-});
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // Aquí silenciamos el error de turbopack que te apareció antes
+  turbopack: {},
+  
+  // Si tienes otras configuraciones (como imágenes), van aquí
+  images: {
+    unoptimized: true,
+  },
+};
+
+module.exports = withPWA(nextConfig);
